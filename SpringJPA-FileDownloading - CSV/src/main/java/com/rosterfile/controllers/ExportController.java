f1eff7659158953f9d449507b479af3241ae2603
Jimmy Ismael Pena <jimmydefender@gmail.com>
2017-10-19 09:20:24 -0400

    Update ExportController.java

--- SpringJPA-FileDownloading - CSV/src/main/java/com/rosterfile/controllers/ExportController.java
+++ SpringJPA-FileDownloading - CSV/src/main/java/com/rosterfile/controllers/ExportController.java
@@ -22,8 +22,8 @@ public class ExportController {
     @Autowired
     RosterFileService rosterFileService;
 
-    private static final String FILE_PATH = "SalesRoster.xlsx";
-    private static final String SALESROSTER_XLSX = "SalesRoster/xlsx";
+    private static final String FILE_PATH = "SalesRoster.csv";
+    private static final String SALESROSTER_CSV = "SalesRoster/csv";
 
     private File getFile() throws FileNotFoundException {
         File file = new File(FILE_PATH);
@@ -38,76 +38,63 @@ public class ExportController {
     * Send the rosterList object to the model, when the model received it the ExcelView class
     * catchs it and creat the Excel file.
     * Look a the line 30 on ExcelView class */
-    @RequestMapping(value = "/exportToExcel")
-    public void exportToExcel( HttpServletResponse response) throws IOException {
+    @RequestMapping(value = "/exportToCSV")
+    public void exportToCSV( HttpServletResponse response) throws IOException {
 
-        // Using XSSF for xlsx format, for xls use HSSF
-        XSSFWorkbook workbook = new XSSFWorkbook();
+        Iterable<Roster> roster=rosterFileService.listAll();
 
-        Sheet studentsSheet = workbook.createSheet("SalesRoster");
-        studentsSheet.setDefaultColumnWidth(30);
+        //Creating the CSV File
+        //Delimiters which has to be in the CSV file
+        char COMMA_DELIMITER = ',';
+        String LINE_SEPARATOR = "\n";
 
-        // create style for header cells
-        CellStyle style = workbook.createCellStyle();
-        Font font = workbook.createFont();
-        font.setFontName("Arial");
-        style.setFillForegroundColor(HSSFColor.BLUE.index);
-        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
-        font.setBoldweight(Font.BOLDWEIGHT_NORMAL);
-        font.setColor(HSSFColor.WHITE.index);
-        style.setFont(font);
+        FileWriter fileWriter=null;
 
-
-        // create header row
-        Row header = studentsSheet.createRow(0);
-        header.createCell(0).setCellValue("ZRT");
-        header.getCell(0).setCellStyle(style);
-        header.createCell(1).setCellValue("Name");
-        header.getCell(1).setCellStyle(style);
-        header.createCell(2).setCellValue("Mailing Address 1");
-        header.getCell(2).setCellStyle(style);
-        header.createCell(3).setCellValue("Mailing Address 2");
-        header.getCell(3).setCellStyle(style);
-        header.createCell(4).setCellValue("City");
-        header.getCell(4).setCellStyle(style);
-        header.createCell(5).setCellValue("State");
-        header.getCell(5).setCellStyle(style);
-        header.createCell(6).setCellValue("ZIP");
-        header.getCell(6).setCellStyle(style);
-        header.createCell(7).setCellValue("Phone Number");
-        header.getCell(7).setCellStyle(style);
-
-        int rowCount = 1;
-        for(Roster user : rosterFileService.listAll()){
-            Row userRow =  studentsSheet.createRow(rowCount++);
-            userRow.createCell(0).setCellValue(user.getZRT());
-            userRow.createCell(1).setCellValue(user.getFULL_NAME());
-            userRow.createCell(2).setCellValue(user.getHOMEADDRESS1());
-            userRow.createCell(3).setCellValue(user.getHOMEADDRESS2());
-            userRow.createCell(4).setCellValue(user.getHOMECITY());
-            userRow.createCell(5).setCellValue(user.getHOMESTATE());
-            userRow.createCell(6).setCellValue(user.getHOMEZIPCODE());
-            userRow.createCell(7).setCellValue(user.getCELL_PHONE());
-        }
-
-
-        //write this workbook in excel file.
         try {
-            FileOutputStream fos = new FileOutputStream(FILE_PATH);
-            workbook.write(fos);
-            fos.close();
+            fileWriter=new FileWriter(FILE_PATH);
 
-            System.out.println(FILE_PATH + " is successfully written");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            List<String> lines= new ArrayList<String>();
+            for (Roster item:roster){
+                fileWriter.append(item.getZRT());
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(item.getFULL_NAME());
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(item.getHOMEADDRESS1());
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(item.getHOMEADDRESS2());
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(item.getHOMECITY());
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(item.getHOMESTATE());
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(item.getHOMEZIPCODE());
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append(LINE_SEPARATOR);
+            }
+
+            System.out.println("Write to CSV file Succeeded!!!");
+        }
+        catch(Exception ee)
+        {
+            ee.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                fileWriter.close();
+            }
+            catch(IOException ie)
+            {
+                System.out.println("Error occured while closing the fileWriter");
+                ie.printStackTrace();
+            }
         }
 
         //-----Downloading the file-----//
         File file = getFile();
         InputStream in = new FileInputStream(file);
-        response.setContentType(SALESROSTER_XLSX);
+        response.setContentType(SALESROSTER_CSV);
         response.setHeader("Content-Disposition", "attachment; filename=" + file.getName());
         response.setHeader("Content-Length", String.valueOf(file.length()));
         FileCopyUtils.copy(in, response.getOutputStream());
