279851ba9503df46c1105ddbca0c7cbbc64e7785
Jimmy Ismael Pena <jimmydefender@gmail.com>
2017-10-19 08:00:22 -0400

    Renaming the directory.

---
+++ SpringJPA-FileDownloadingExample - XLSX/target/classes/static/js/RosterFunctions.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Jimmy Pena on 8/24/2017.
+ */
+function searchSalesman() {
+    var zrt=document.getElementById('ZRT').value;
+    var name=document.getElementById('salesmanName').value;
+
+    if (zrt!=""){
+        window.location.href="/searchByZRT/"+zrt;
+    }
+    else {
+        if (name!="") {
+            window.location.href="/searchByName/"+name;
+        }
+        else{
+            window.location.href="/";
+        }
+    }
+
+}
+function exportToExcel() {
+    window.location.href="/exportToExcel";
+}
